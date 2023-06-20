a81c43b45c594ee899576f5779a5977090ee4093
ccsweets <tramp0102@gmail.com>
2023-06-20 14:41:02 +0900

    2023

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup ./run_by_param.sh ccsweets/moara-12.8b-wv_namu_stt_0605 > basaran.log &
--- run_by_param.sh
+++ run_by_param.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -m basaran MODEL=$1 PORT=30098 trust_remote_code=True > basaran.log
+MODEL=$1 PORT=30098 trust_remote_code=True python -m basaran
